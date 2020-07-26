12f02172fef3380cde7c4777fca33cca08d557d8
Hamza <hamza.khalil3343@gmail.com>
2020-07-26 12:37:41 +0500

    leaders and promotions are Added ->  ASS 1

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 
 
 const server = http.createServer(app);
