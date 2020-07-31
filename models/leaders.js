5c00ce43cc799106d3fa495500313a76f56d3f96
Hamza <hamza.khalil3343@gmail.com>
2020-07-31 11:26:24 +0500

    leaders and promotions schema is added Ass->2

---
+++ models/leaders.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    }, 
+    designation: {
+        type: String,
+        default: ''
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+   
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    }
+}, {
+    timestamps: true
+});
+
+var leaders = mongoose.model('leaders', leaderSchema);
+
+module.exports = leaders;
--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
 
@@ -7,39 +10,72 @@ leaderRouter.use(bodyParser.json());
 
 
 leaderRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('Will send all the leaders to you!');
+    leaders.find({})
+    .then((leaders) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leaders);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    leaders.create(req.body)
+    .then((leader) => {
+        console.log('leader  Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /leader');
+    res.end('PUT operation not supported on /leaders');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all leader');
+    leaders.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 });
-leaderRouter.route('/:leaderId')
-.get( (req,res,next) => {
-    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+
+leaderRouter.route('/:leaderid')
+.get((req,res,next) => {
+    leaders.findById(req.params.leaderid)
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-.post( (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /leaders/'+ req.params.leaderid);
 })
-.put( (req, res, next) => {
-  res.write('Updating the leader: ' + req.params.leaderId + '\n');
-  res.end('Will update the leader: ' + req.body.name + 
-        ' with details: ' + req.body.description);
+.put((req, res, next) => {
+    leaders.findByIdAndUpdate(req.params.leaderid, {
+        $set: req.body
+    }, { new: true })
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-.delete(  (req, res)=> {
-    res.end('Deleting leader: ' + req.params.leaderId);
+.delete((req, res, next) => {
+    leaders.findByIdAndRemove(req.params.leaderid)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 
